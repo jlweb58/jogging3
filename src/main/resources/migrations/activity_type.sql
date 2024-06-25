9d014dc78a2da95428f6e6e241b94ff784eeca1b
John Webber <j.webber@roadsurfer.com>
2024-06-25 14:58:07 +0200

    Finished data migration

--- src/main/resources/migrations/activity_type.sql
+++ src/main/resources/migrations/activity_type.sql
@@ -1,3 +1,19 @@
 
 ALTER TABLE run
 RENAME TO activity;
+
+ALTER TABLE activity
+ADD COLUMN activity_type VARCHAR(16);
+
+UPDATE activity
+SET activity_type = 'RUN'
+WHERE course NOT IN ('Rad', 'Swim');
+
+UPDATE activity
+SET activity_type = 'SWIM'
+where course = 'Swim';
+
+UPDATE activity
+SET activity_type = 'BIKE_RIDE'
+where course = 'Rad';
+
